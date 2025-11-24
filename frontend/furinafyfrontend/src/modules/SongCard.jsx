794a77f2ef1bb5b0904b367bd6067c653da9a0bf
richiii@umich.edu <richiii@umich.edu>
2025-11-24 16:48:32 -0500

    SongCard WIP

---
+++ frontend/furinafyfrontend/src/modules/SongCard.jsx
@@ -0,0 +1,14 @@
+
+
+function SongCard(){
+
+
+    return(
+        <div className="songCard">
+            <img src="" alt="cardImg"></img>
+            <h2>Test Card</h2>
+            <p>Test Description 1 2 3</p>
+        </div>
+);}
+
+export default SongCard
