12e70fb8d39644619f162c4990130780619bf57e
richiii@umich.edu <richiii@umich.edu>
2025-11-24 19:13:28 -0500

    Song Card done

--- frontend/furinafyfrontend/src/modules/SongCard.jsx
+++ frontend/furinafyfrontend/src/modules/SongCard.jsx
@@ -1,13 +1,17 @@
-
+import songImg from '../assets/kitty.jpg'
 
 function SongCard(){
 
+    const handleClick = () => { 
+        console.log("Clicked"); 
+    }
 
     return(
         <div className="songCard">
-            <img src="" alt="cardImg"></img>
-            <h2>Test Card</h2>
-            <p>Test Description 1 2 3</p>
+            <img className='songImage' src={songImg} alt="songImg"></img>
+            <h2 className='songTitle'>Name</h2>
+            <p className='songText'>Artist</p>
+            <button className='songButton' onClick={() => handleClick()}>Click</button>
         </div>
 );}
 
