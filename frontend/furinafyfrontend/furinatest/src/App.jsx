a48f1a649bb1c6c89c1accd4dd6047ca8552b5c7
Princess Xia <Princess Xia>
2025-12-05 20:27:18 -0500

    please work i beg yall

---
+++ frontend/furinafyfrontend/furinatest/src/App.jsx
@@ -0,0 +1,18 @@
+import { useState } from 'react'
+import MainScreen from "./modules/MainScreen";
+import './styles/App.css'
+import SongCard from './modules/SongCard.jsx'
+
+function App() {
+  const [count, setCount] = useState(0)
+  
+  return (
+    <>
+      {/*<SongCard/>*/}
+      <MainScreen/>
+      <p>pp</p>
+    </>
+  )
+}
+
+export default App
