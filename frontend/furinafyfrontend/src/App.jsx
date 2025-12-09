cb3bc6bf7499d7d74c9c3cce95717d87b837f4fd
Princess Xia <Princess Xia>
2025-12-09 18:52:44 -0500

    adding rirchard's stuff to my branch

--- frontend/furinafyfrontend/src/App.jsx
+++ frontend/furinafyfrontend/src/App.jsx
@@ -2,6 +2,8 @@ import { useState } from 'react'
 import MainScreen from "./modules/MainScreen";
 import './styles/App.css'
 import SongCard from './modules/SongCard.jsx'
+import './styles/SongCard.css'
+
 
 function App() {
   const [count, setCount] = useState(0)
