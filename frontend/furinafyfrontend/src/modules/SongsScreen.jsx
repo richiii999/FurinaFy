9cd8b422bc916def2d0d098a280b095b59f6a7d9
Kyle <kyle12140@gmail.com>
2025-12-09 17:01:52 -0500

    connected song card to song screen

--- frontend/furinafyfrontend/src/modules/SongsScreen.jsx
+++ frontend/furinafyfrontend/src/modules/SongsScreen.jsx
@@ -1,7 +1,15 @@
+import SongCard from "./SongCard";
 function SongsScreen({ onClick }){
     return (
+    /*temporary rn replaces these with song cards****/
     <div>
-        <button onClick={onClick}>Songs</button>
+    <button onClick={onClick}><SongCard/></button>
+    <button onClick={onClick}><SongCard/></button>
+    <button onClick={onClick}><SongCard/></button>
+    <button onClick={onClick}><SongCard/></button>
+    <button onClick={onClick}><SongCard/></button>
+    <button onClick={onClick}>+</button>    
+    <button onClick={onClick}>Songs</button>
     </div>
     );
 };
