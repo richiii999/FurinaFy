db3a79284b1de0bd3c4f375af49a38b67b697835
richiii@umich.edu <richiii@umich.edu>
2025-12-09 17:02:49 -0500

    put my card back

--- frontend/furinafyfrontend/src/App.jsx
+++ frontend/furinafyfrontend/src/App.jsx
@@ -1,14 +1,16 @@
 import { useState } from 'react'
 import MainScreen from "./modules/MainScreen";
 import './styles/App.css'
+
 import SongCard from './modules/SongCard.jsx'
+import './styles/songCard.css'
 
 function App() {
   const [count, setCount] = useState(0)
   
   return (
     <>
-      {/*<SongCard/>*/}
+      <SongCard/>
       <MainScreen/>
       <p>pp</p>
     </>
