720f98d482a5a98d8cea647d53f37db534428ed0
caleb7198 <33532162+caleb7198@users.noreply.github.com>
2025-12-10 16:18:25 -0500

    Back End
    Added backend support using express, connecting to local instance of mongodb

---
+++ backend/furinafybackend/models/songModel.js
@@ -0,0 +1,27 @@
+const mongoose = require('mongoose');
+
+const songSchema = mongoose.Schema({
+    title : {
+        type : String,
+        required : [true, "Please enter a title"]
+    },
+    length : {
+        type : String,
+        required : [true, "Please enter a length"]
+    },
+    picture : {
+        type : String,
+        required : [true, "Please enter base64 url of picture"]
+    },
+    song : {
+        type : String,
+        required : [true, "Please enter base64 url of .mp3 song"]
+    }
+    },
+{
+    timestamps : false
+})
+
+const Song = mongoose.model('Song', songSchema);
+
+module.exports = Song;
